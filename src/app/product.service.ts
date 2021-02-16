fc59ecf2b62f275a9ebdf8119b66665ff7b262c8
decraemerdante@gmail.com <decraemerdante@gmail.com>
2021-02-16 16:09:18 +0100

    productlist

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,11 +4,14 @@ import { catchError } from 'rxjs/operators';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = "../assets/album.json";
+
+  private _productsUrl: string = '../assets/products.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
@@ -17,4 +20,9 @@ export class ProductService {
     );
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(
+      response => <Product[]>response.json()
+    );
+  }
 }
